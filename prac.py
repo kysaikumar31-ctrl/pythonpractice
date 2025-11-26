06a3611d1cdce9635fe96c5ae4736c4c5e520f3e
Admin <kysaikumar31@gmail.com>
2025-11-26 08:54:33 +0530

    added a star pattern

--- prac.py
+++ prac.py
@@ -5,6 +5,7 @@ for row in range(1,6):
         print("*",end=" ")
     print()
 
+
 for i in range(1, 6):
     for j in range(6-i):
         print(" ",end="")
@@ -21,5 +22,13 @@ for i in range(1, rows + 1):
     for j in range(1, i + 1):
         print(c, end=" ")
         c = c * (i - j) // j
+    print()
+
+rows = int(input("Enter the number of rows: "))
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print("*", end=" ")
     print()
 
